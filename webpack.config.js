dd4ace326c06a8c2e35b95e58411c01f6b753b23
reblws <enrico.baculinao@gmail.com>
2017-04-02 12:46:46 -0400

    Initialize webpack stuff for hello

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./index.js",
+    output: {
+      path: __dirname + '/dist',
+      filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
+    }
+};
