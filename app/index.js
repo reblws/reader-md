5fa75c81ed7a6596d0fb132f09bbd71830ff88a8
reblws <enrico.baculinao@gmail.com>
2017-04-10 22:46:15 -0400

    Update typo on index.js and md_reader.html

--- app/index.js
+++ app/index.js
@@ -9,34 +9,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   console.log("DOM Content loaded");
 
-  var markdownViewer = document.getElementById('markdownViewer');
+  const markdownViewer = document.getElementById('markdownViewer');
   if (!markdownViewer) {
-    console.log('Markdown Viewer not foun')
     return;
   }
 
   // postMessage is how we communicate with Standard Notes
   if (window.parent != window) {
     window.parent.postMessage({status: 'ready'}, '*');
-    console.log('message posted')
   };
-
 
   // Set some props from the data we just received from SN.
   // This is where we output the message onto html I think.
   window.addEventListener("message", function(event){    
-    console.log("message received");
-
     window.noteText = event.data.text || "";
     window.noteId = event.data.id;
     markdownViewer.innerHTML = markdownParser.render(window.noteText);
   }, false);
 
-
-
-
   // Now just to set up markdownParser.set
-  var markdownParser = new Remarkable({
+  const markdownParser = new Remarkable({
     breaks: true,
     typographer: true,
     // This highlights languages
@@ -56,11 +48,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   flowtype(markdownViewer, {
-    maxWidth: '800px',
+    maxWidth: '850px',
     minWidth: '300px',
-    fontRatio: 30,
+    lineRatio: 1.45,
     min: 14,
-    max: 22
+    max: 21
+
   });
 
 
