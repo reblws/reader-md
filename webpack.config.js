9de2191a7208c3f1b05d4a9018124a941ecd6758
reblws <enrico.baculinao@gmail.com>
2017-04-11 20:48:55 -0400

    Add md styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
+const webpack = require('webpack')
+
 module.exports = {
     entry: "./app/index.js",
     output: {
       path: __dirname + '/dist',
       filename: "bundle.min.js"
     },
+    plugins: [
+      new webpack.DefinePlugin({
+        '__DEV__': false
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: true
+        }
+      })
+    ]
 };
