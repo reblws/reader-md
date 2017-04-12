913182ecfd5d832b1b9c993188734ef4f83b46aa
reblws <enrico.baculinao@gmail.com>
2017-04-11 20:48:28 -0400

    Tweak flowtype width

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
-import flowtype from 'flowtype-js';
-import Remarkable from 'remarkable';
-import hljs from 'highlight.js';
+const flowtype = require('flowtype-js');
+const Remarkable = require('remarkable');
+const hljs = require('highlight.js');
 
 // Wait for DOM content to load
 
@@ -9,15 +9,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
   console.log("DOM Content loaded");
 
   const markdownViewer = document.getElementById('markdownViewer');
+
   if (!markdownViewer) {
     return;
   }
 
   // Now just to set up markdownParser.set
   const markdownParser = new Remarkable({
+    html: true,
     breaks: true,
     typographer: true,
-    // This highlights languages
+    quotes: '“”‘’',
     highlight: function (str, lang) {
       if (lang && hljs.getLanguage(lang)) {
         try {
@@ -47,12 +49,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }, false);
 
   flowtype(markdownViewer, {
-    minimum   : 500,
-    maximum   : 1200,
-    minFont   : 12,
-    maxFont   : 40,
-    fontRatio : 30
-  );
+      minimum   : 500,
+      maximum   : 900,
+      minFont   : 12,
+      maxFont   : 40,
+      fontRatio : 30
+    });
 });
 
 
